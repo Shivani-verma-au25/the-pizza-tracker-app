bf127021e1e777fd95d490ab35846eefaad630c7
Shivani Verma <shivani.vr.verma@gmail.com>
2026-02-03 00:51:15 +0530

    socket connection done in frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,46 @@ import dotenv from 'dotenv'
 dotenv.config();
 import { app } from './src/app.js';
 import { connectToDb } from './src/db/databse.js';
+import http from 'http'
+import { Server } from 'socket.io';
 
 
 const port = process.env.PORT || 8000;
 
+//  http + socket
+
+// server 
+const server = http.createServer(app);
+
+// socket
+const io = new Server(server ,{
+    cors :{
+        origin : `http://localhost:5173`,
+        credentials : true
+    }
+});
+
+console.log("🔥 Socket server initialized");
+
+//  set to global variable
+global.io = io;
+
+io.on('connection',(socket) =>{
+    console.log("Socket connected :",socket.id);
+
+    socket.on("joinOrderRoom" , (orderId)=>{
+        socket.join(orderId);
+    })
+});
+
+
+//  databse connection 
 connectToDb().then(()=>{
-    app.listen(port ,()=> {
-    console.log(`Server is running on port ${port}`);
+    server.listen(port ,()=> {
+    console.log(`Server + Socket running on port ${port} ${port}`);
     })
 }).catch((error)=>{
     console.log("Failed to connect to the database ",error);
 })
+
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
@@ -8,10 +8,33 @@ import Signup from './pages/Signup'
 import { Toaster } from './components/ui/sonner'
 import LoadingPage from './components/LoadingPage'
 import { useSelector } from 'react-redux'
+import { socket } from './utils/socket'
 
 function App() {
   const {userLoading} = useSelector(state => state?.auth)
     // if (userLoading) return <LoadingPage text="Authenticating" />;
+
+  // socket connection
+   useEffect(()=>{
+    socket.connect();
+
+    socket.on('connect' ,() =>{
+      console.log("Socket connected :-",socket.id);
+    })
+
+  // disconnecting socket
+    socket.on("disconnect" ,() =>{
+      console.log("Sockect disconnected");
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.disconnect();
+    };
+    
+   },[])
+
   return (
     <>
     <Navbar />
